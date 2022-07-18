d1ad315a2db9e68fec2ef6697a5743b769bf51f3
Andrew Vu <andrew.vu@drivetime.com>
2022-07-18 14:26:38 -0500

    feat: add stiching bc api

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
+const { fetch } = require('cross-undici-fetch');
 const { ApolloServer } = require('apollo-server');
 const { ApolloGateway } = require('@apollo/gateway');
-const { readFileSync } = require('fs');
+const { introspectSchema, wrapSchema } = require('@graphql-tools/wrap');
+const { print } = require('graphql');
 
-const supergraphSdl = readFileSync('./supergraph.graphql').toString();
+const run = async () => {
+    // stiching BC API over
+    const executor = async ({ document, variables }) => {
+        const query = print(document)
+        const fetchResult = await fetch('http://localhost:5002/graphql', { // BC API uri for stiching
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query, variables })
+        })
+        return fetchResult.json()
+    }
 
-const gateway = new ApolloGateway({
-    supergraphSdl
-});
+    const bcApiSchema = await  wrapSchema({
+        schema: await introspectSchema(executor),
+        executor
+    });
 
-const server = new ApolloServer({
-    gateway,
-});
+// Create gateway for federation
+    const gateway = new ApolloGateway({
+        // serviceList is deprecated. Only using it for POC purposes
+        serviceList: [
+            { name: 'locations', url: 'https://flyby-locations-sub.herokuapp.com/' },
+            { name: 'reviews', url: 'https://flyby-reviews-sub.herokuapp.com/' },
+            { name: 'loan-mod-api', url: 'http://localhost:5154/graphql' }, // add loan mod uri here
+        ],
+    });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀 Gateway ready at ${url}`);
-}).catch(err => {console.error(err)});
+    const server = new ApolloServer({
+        gateway,
+        // schema: bcApiSchema,
+        // Subscriptions are not currently supported in Apollo Federation
+        subscriptions: false
+    });
+
+    server.listen().then(({ url }) => {
+        console.log(`🚀 Gateway ready at ... ${url}`);
+    });
+}
+
+try {
+    run();
+} catch (err) {
+    console.error(err);
+}
