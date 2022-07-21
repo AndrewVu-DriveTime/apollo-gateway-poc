375bc95c2f76d6427e752becc989844fbeb35935
Andrew Vu <andrew.vu@drivetime.com>
2022-07-21 10:32:37 -0500

    chore: testing stitching federation

--- src/schema-services/federation-services.js
+++ src/schema-services/federation-services.js
@@ -1,29 +1,35 @@
-const { stitchSchemas } = require('@graphql-tools/stitch');
-const { federationToStitchingSDL, stitchingDirectives } = require('@graphql-tools/stitching-directives')
+const { stitchSchemas } = require("@graphql-tools/stitch");
+const {
+  federationToStitchingSDL,
+  stitchingDirectives,
+} = require("@graphql-tools/stitching-directives");
 
-const { buildSchema } = require('graphql');
-const { makeRemoteExecutor } = require('../lib');
-const stitchingConfig = stitchingDirectives()
+const { buildSchema } = require("graphql");
+const { makeRemoteExecutor } = require("../lib");
+const stitchingConfig = stitchingDirectives();
 
 // https://www.graphql-tools.com/docs/schema-stitching/stitch-federation
 async function fetchFederationSubchema(executor) {
-    const { data } = await executor({ document: '{ _service { sdl } }' });
-    const sdl = federationToStitchingSDL(data._service.sdl, stitchingConfig);
-    return {
-        schema: buildSchema(sdl),
-        executor,
-    };
+  const { data } = await executor({ document: "{ _service { sdl } }" });
+  const sdl = federationToStitchingSDL(data._service.sdl, stitchingConfig);
+  return {
+    schema: buildSchema(sdl, { assumeValidSDL: true }),
+    executor,
+  };
 }
 
 async function makeGatewaySchema() {
-    return stitchSchemas({
-        subschemaConfigTransforms: [stitchingConfig.stitchingDirectivesTransformer],
-        subschemas: await Promise.all([
-            fetchFederationSubchema(makeRemoteExecutor('http://localhost:5154/graphql')),
-            fetchFederationSubchema(makeRemoteExecutor('http://localhost:5160/graphql')),
-        ])
-    });
+  return stitchSchemas({
+    subschemaConfigTransforms: [stitchingConfig.stitchingDirectivesTransformer],
+    subschemas: await Promise.all([
+      fetchFederationSubchema(
+          makeRemoteExecutor("https://flyby-locations-sub.herokuapp.com/")
+      ),
+      fetchFederationSubchema(
+        makeRemoteExecutor("https://flyby-reviews-sub.herokuapp.com/")
+      ),
+    ]),
+  });
 }
 
-module.exports =  makeGatewaySchema;
-
+module.exports = makeGatewaySchema;
