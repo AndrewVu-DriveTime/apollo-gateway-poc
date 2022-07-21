c93c53e16f7cadba858ea8755ac69a681c7d9a6c
Andrew Vu <andrew.vu@drivetime.com>
2022-07-21 16:56:51 -0500

    chore: use apollo studio for the gateway

--- index.js
+++ index.js
@@ -6,41 +6,43 @@ const { stitchSchemas } = require("@graphql-tools/stitch");
 const { delegateToSchema } = require("@graphql-tools/delegate");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const {ApolloServer} = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
+
+require("dotenv").config();
 
 const run = async () => {
   // 1. Create gateway and federation schema
   // Authentication and other options: https://www.apollographql.com/docs/federation/api/apollo-gateway
-  const gateway = new ApolloGateway({
-    // serviceList is deprecated. Only using it for POC purposes
-    serviceList: [
-      { name: "locations", url: "https://flyby-locations-sub.herokuapp.com/" },
-      { name: "reviews", url: "https://flyby-reviews-sub.herokuapp.com/" },
-      { name: "loan-mod-api", url: "http://localhost:5154/graphql" }, // add loan mod uri here
-    ],
-  });
+  const gateway = new ApolloGateway();
+
   const federationServer = new ApolloServer({
     gateway,
-    subscription: false
+    // Subscriptions are not currently supported in Apollo Federation
+    subscriptions: false,
   });
+
   // start local federation server for executor to work
-  await federationServer.listen({
-    port: 4001
-  });
+  await federationServer
+    .listen({
+      port: 4001,
+    })
+    .then(({ url }) => {
+      console.log(`🚀 Gateway ready at ${url}`);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   const federationExecutor = async ({ document, variables }) => {
     const query = print(document);
-    const fetchResult = await fetch(
-        "http://0.0.0:4001/graphql",
-        {
-          // BC API uri for stiching
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ query, variables }),
-        }
-    );
+    const fetchResult = await fetch("http://0.0.0:4001/graphql", {
+      // BC API uri for stiching
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query, variables }),
+    });
     return fetchResult.json();
   };
 
@@ -52,7 +54,8 @@ const run = async () => {
   // 2. Creating BC API Schema
   const executor = async ({ document, variables }) => {
     const query = print(document);
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJlZjZhZWZlYi1lMzVkLTQ1MDctYWE2Mi0zZmFlNTc1YjgyYzkiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xYWZmMDY2OS1lZTVmLTQwYjgtOTgwMC1iNWVjNGYzOWM0OGUvIiwiaWF0IjoxNjU4MjU4ODI1LCJuYmYiOjE2NTgyNTg4MjUsImV4cCI6MTY1ODI2MjcyNSwiYWlvIjoiRTJaZ1lIaTZjMkxhMW8vcm5MeSt4UG9IVG5QK0N3QT0iLCJhcHBpZCI6IjU1ZjEwYmE1LWJlMmQtNDMwMy1iZjFmLTEyZmI2MTZiYTMyZCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzFhZmYwNjY5LWVlNWYtNDBiOC05ODAwLWI1ZWM0ZjM5YzQ4ZS8iLCJvaWQiOiI5MjdjMTE4Ny1kYjk0LTQzYjEtOGQyOC0xZmEzNTJmMGM2NDUiLCJyaCI6IjAuQVc0QWFRYl9HbF91dUVDWUFMWHNUem5FanV2dmF1OWQ0d2RGcW1JX3JsZGJnc2x1QUFBLiIsInJvbGVzIjpbIkFjY291bnRfb2M6RHJpdmVUaW1lX2xwOkRyaXZlVGltZV9zcDpEcml2ZVRpbWUiLCJBY2NvdW50X29jOkdPRklOQU5DSUFMX2xwOkdPRklOQU5DSUFMX3NwOkRyaXZlVGltZSIsIkFjY291bnRfb2M6Q0FSVkFOQV9scDpHT0ZJTkFOQ0lBTF9zcDpEcml2ZVRpbWUiLCJBY2NvdW50X29jOkNBUlZBTkFfbHA6QWxseV9zcDpEcml2ZVRpbWUiLCJBY2NvdW50U3RhdHVzX0FMTCIsIkFjY291bnRfb2M6RHJpdmVUaW1lX2xwOkFsbHlfc3A6RHJpdmVUaW1lIiwiQWNjb3VudF9vYzpEcml2ZVRpbWVfbHA6U2N1c2Ffc3A6U2N1c2EiLCJBY2NvdW50X29jOkdvRmlfbHA6R09GSU5BTkNJQUxfc3A6RHJpdmVUaW1lIl0sInN1YiI6IjkyN2MxMTg3LWRiOTQtNDNiMS04ZDI4LTFmYTM1MmYwYzY0NSIsInRpZCI6IjFhZmYwNjY5LWVlNWYtNDBiOC05ODAwLWI1ZWM0ZjM5YzQ4ZSIsInV0aSI6Ii1iWW8yMnlGUVVLakl5RHpCZ3JkQUEiLCJ2ZXIiOiIxLjAifQ.MeON7-X_jTur4YNUqqPVgixYeUlSBG3_OIoI9A78S5s-7FQ8qvAL5bYog7kJ1nNWDmTp1bjILV7J2sE6R0V06lBNxGg2Ui0puGn8m_5W84ik7wMssiakHNqoGU7m0BdA0dabRNCQmVB4IePMv8OLbVHdhL-09lxv_AgYFzGMo9Oy5obJLES_N1P7BxY9PWq-k27VNGgPKplITUW_vh1mPDuyUR-WoYiGWabWroaNt9F3df9nsDN-93AK2WT0-IbSM6RPufwpcD5JAeELaQCxk1IqXqxbbCEhjs2MPjVJnYvoD2pxQMXJxYOShln3KhZE-lIdl0wcfT21U2yL3wlKbg';
+    const token =
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJlZjZhZWZlYi1lMzVkLTQ1MDctYWE2Mi0zZmFlNTc1YjgyYzkiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xYWZmMDY2OS1lZTVmLTQwYjgtOTgwMC1iNWVjNGYzOWM0OGUvIiwiaWF0IjoxNjU4NDQwMTM5LCJuYmYiOjE2NTg0NDAxMzksImV4cCI6MTY1ODQ0NDAzOSwiYWlvIjoiRTJaZ1lEaXptanRRdk1udjBIZFp3WFVmQk9ZZUFRQT0iLCJhcHBpZCI6IjU1ZjEwYmE1LWJlMmQtNDMwMy1iZjFmLTEyZmI2MTZiYTMyZCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzFhZmYwNjY5LWVlNWYtNDBiOC05ODAwLWI1ZWM0ZjM5YzQ4ZS8iLCJvaWQiOiI5MjdjMTE4Ny1kYjk0LTQzYjEtOGQyOC0xZmEzNTJmMGM2NDUiLCJyaCI6IjAuQVc0QWFRYl9HbF91dUVDWUFMWHNUem5FanV2dmF1OWQ0d2RGcW1JX3JsZGJnc2x1QUFBLiIsInJvbGVzIjpbIkFjY291bnRfb2M6RHJpdmVUaW1lX2xwOkRyaXZlVGltZV9zcDpEcml2ZVRpbWUiLCJBY2NvdW50X29jOkdPRklOQU5DSUFMX2xwOkdPRklOQU5DSUFMX3NwOkRyaXZlVGltZSIsIkFjY291bnRfb2M6Q0FSVkFOQV9scDpHT0ZJTkFOQ0lBTF9zcDpEcml2ZVRpbWUiLCJBY2NvdW50X29jOkNBUlZBTkFfbHA6QWxseV9zcDpEcml2ZVRpbWUiLCJBY2NvdW50U3RhdHVzX0FMTCIsIkFjY291bnRfb2M6RHJpdmVUaW1lX2xwOkFsbHlfc3A6RHJpdmVUaW1lIiwiQWNjb3VudF9vYzpEcml2ZVRpbWVfbHA6U2N1c2Ffc3A6U2N1c2EiLCJBY2NvdW50X29jOkdvRmlfbHA6R09GSU5BTkNJQUxfc3A6RHJpdmVUaW1lIl0sInN1YiI6IjkyN2MxMTg3LWRiOTQtNDNiMS04ZDI4LTFmYTM1MmYwYzY0NSIsInRpZCI6IjFhZmYwNjY5LWVlNWYtNDBiOC05ODAwLWI1ZWM0ZjM5YzQ4ZSIsInV0aSI6ImN2cHJ6UTgtM2tpYTRfUnE0bExtQUEiLCJ2ZXIiOiIxLjAifQ.M3P3kDy1DXswNLN4b5_wtbMigPjraYSCDRaWIhUWtd7aJv3soEaAPZQDbofJVgDU8zc9zrSD0b4bFAlvNOgX1DxwaMIBITstLJswlYQ35uO5w5IqzQ-4FcSnTYDTj7YcoT7zSoGp4fYso4UVTwUSkWpvfzYfzDzWSiJ626J37baMi3_qS0FtxUGbvzeEcXQD9IyUrxrED4xK12ZzG_PiccAPgiAYyrbXXvEiurw6VoOmnDPSN2P1PuqPd_kQoztXeJi5b3VdbtQtOHAI6rKsu1tKj1QmRkLrX0o45RSAzc1t9KrWD_icj1rDrguh5N0i2ziIteftskox9sWK2YqOsQ";
     const fetchResult = await fetch(
       "https://api-test.bridgecrest.com/graphql",
       {
@@ -61,7 +64,7 @@ const run = async () => {
         headers: {
           "Content-Type": "application/json",
           // Need new Authorization after it's expired
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({ query, variables }),
       }
@@ -83,7 +86,7 @@ const run = async () => {
         schema: bcApiSchema,
       },
       {
-        schema: gatewaySchema
+        schema: gatewaySchema,
         // schema: gateway.schema,
         // executor: gateway.executor, // TODO: this executor is not resolving anything,
       },
@@ -91,17 +94,17 @@ const run = async () => {
     //https://www.graphql-tools.com/docs/schema-stitching/stitch-schema-extensions
     typeDefs: /* GraphQL */ `
       extend type SelfServiceCustomerType {
-        notes: [Note]
+        reviews: [Review]
       }
     `,
     resolvers: {
       SelfServiceCustomerType: {
-        notes: {
+        reviews: {
           resolve(parent, args, context, info) {
             return delegateToSchema({
               schema: gatewaySchema,
               operation: "query",
-              fieldName: "notes",
+              fieldName: "latestReviews",
               context,
               info,
             });
